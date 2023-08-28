1cba95dd7956c13326b9c88b83a1672884b27ed1
YMYdashuaibi <127722366+YMYdashuaibi@users.noreply.github.com>
2023-08-28 10:25:03 +0800

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -1,13 +1,24 @@
 #include"enemy.h"
 class enemy(){
   public:
-  
+  enemy(int hp,int atk,int def,int money,int exp,int item):Hp(hp),Atk(atk),Def(def),Money_drop(money),Exp_drop(exp){}
   getHp(){}
+  setHp(){}
   getAtk(){}
+  setAtk(){}
+  getDef(){}
+  setDef(){}
+  getmoney(){}
+  setmoney(){}
+  getexp(){}
+  setexp(){}
+  getitem(){}
+  setitem(){}
   private:
   int Hp;
   int Atk;
   int Def;
   int Money_drop;
   int Exp_drop;
+  int Items_drop;
 }
