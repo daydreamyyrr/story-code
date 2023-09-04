17d46d8b32a4681add613bc619b3cd3b5dd76790
daydreamyyrr <118284831+daydreamyyrr@users.noreply.github.com>
2023-09-04 08:35:30 +0800

    Update store.h

--- store.h
+++ store.h
@@ -1,2 +1,10 @@
 #include<iostream>
 using namespace std;
+class store
+{
+public:
+	store();
+protected:
+	int needmoney[22] = { 450,300,500,450,500,400,250,350,400,100,200,350,500,700,30,80,20,20,120,100,30,30 };
+	bool st[22] = { 0 };
+};
