b26e499e02eb5d7bb06d610ed9c66807f43351a5
ymy517 <127722366+ymy517@users.noreply.github.com>
2023-09-05 14:20:52 +0800

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -4,9 +4,9 @@ enemy::enemy(int flag) :id(flag)
 	switch (flag)
 	{//hp100-1000 atk20-100 def20-100 money10-70 exp10-50 item0
 	case 1:
-		this->enemy::enemy("缢死鬼", 400, 24, 30, 15, 80, 0); break;
+		this->enemy::enemy("缢死鬼", 400, 24, 30, 15, 20, 0); break;
 	case 2:
-		this->enemy::enemy("水鬼", 250, 20, 40, 20, 20, 0); break;
+		this->enemy::enemy("水鬼", 250, 20, 40, 20, 20, 0); break;\\云中池敌人
 	case 3:
 		this->enemy::enemy("拘魂鬼", 300, 35, 40, 40, 30, 0); break;
 	case 4:
@@ -14,15 +14,24 @@ enemy::enemy(int flag) :id(flag)
 	case 5:
 		this->enemy::enemy("厉鬼", 500, 50, 40, 40, 40, 0); break;
 	case 6:
-		this->enemy::enemy("伥鬼", 450, 35, 35, 35, 25, 0); break;
+		this->enemy::enemy("伥鬼", 450, 35, 35, 70, 25, 0); break;\\三风山敌人
 	case 7:
-		this->enemy::enemy("疟疾鬼", 700, 60, 70, 70, 45, 0); break;
+		this->enemy::enemy("疟疾鬼", 700, 60, 70, 50, 45, 0); break;\\弘阳镇敌人
 	case 8:
-		this->enemy::enemy("僵尸", 1000, 40 ,40 ,30 , 50 ,0);break;
+		this->enemy::enemy("王大爷（妖魔缠身）",300,30,20,50,20,0);break;\\清风镇敌人
 	case 9:
-		this->enemy::enemy("丧失",800,60,30,50,60,0);break;
+		this->enemy::enemy("巫婆（炼蛊之人）",250,45,25,50,25,0);break;\\陷土沼泽敌人
 	case 10:
-		
+		this->enemy::enemy("高大强壮的怪物",300,35,40,30,20,0);break;\\阴邪敌人
+	case 11:
+		this->enemy::enemy("骷髅",400,40,40,50,40,0);break;\\阴邪敌人
+	case 12:
+		this->enemy::enemy("山魈",500,50,55,100,60,0);break;\\奇龙镇敌人
+	case 13:
+		this->enemy::enemy("魑、魅、魍魉",200,50,50,200,60,0);break;\\迷雾森林
+	case 14:
+		this->enemy::enemy("饕餮",1000,70,75,1000,100,)0;break;\\云龙峰
+
 	}
 }
 enemy::enemy(string name, int hp, int atk, int def, int money, int exp, int item) : enemy_Name(name), Hp(hp), Atk(atk), Def(def), Money_drop(money), Exp_drop(exp){}
