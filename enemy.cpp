0be8982b8d02a8229e02fd9cea98764ff15329ef
zht2004 <142766364+zht2004@users.noreply.github.com>
2023-09-05 14:11:02 +0800

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -4,7 +4,7 @@ enemy::enemy(int flag) :id(flag)
 	switch (flag)
 	{//hp100-1000 atk20-100 def20-100 money10-70 exp10-50 item0
 	case 1:
-		this->enemy::enemy("缢死鬼", 400, 24, 30, 15, 20, 0); break;
+		this->enemy::enemy("缢死鬼", 400, 24, 30, 15, 80, 0); break;
 	case 2:
 		this->enemy::enemy("水鬼", 250, 20, 40, 20, 20, 0); break;
 	case 3:
@@ -17,6 +17,12 @@ enemy::enemy(int flag) :id(flag)
 		this->enemy::enemy("伥鬼", 450, 35, 35, 35, 25, 0); break;
 	case 7:
 		this->enemy::enemy("疟疾鬼", 700, 60, 70, 70, 45, 0); break;
+	case 8:
+		this->enemy::enemy("僵尸", 1000, 40 ,40 ,30 , 50 ,0);break;
+	case 9:
+		this->enemy::enemy("丧失",800,60,30,50,60,0);break;
+	case 10:
+		
 	}
 }
 enemy::enemy(string name, int hp, int atk, int def, int money, int exp, int item) : enemy_Name(name), Hp(hp), Atk(atk), Def(def), Money_drop(money), Exp_drop(exp){}
