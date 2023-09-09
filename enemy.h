1f4dac26421410582388f36e4bb8075e84de484a
daydreamyyrr <118284831+daydreamyyrr@users.noreply.github.com>
2023-09-09 23:13:35 +0800

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -5,8 +5,9 @@ class enemy
 {
 public:
 	enemy(int flag);
-	enemy(string name, int hp, int atk, int def, int money, int exp, int item) ;
 	string getname();//获得名字
+	void set(int id);
+	void setenemy(string name, int hp, int atk, int def, int money, int exp, int item);
 	void setname(string m_name);//修改名字
 	int getHp();
 	void setHp(int m_hp);
@@ -20,13 +21,17 @@ public:
 	void setexpNums(int m_exp);
 	int getitem();
 	void setitem(int m_item);
+	void enemy_panel();
+	int getdrop();
+	void defdebuff(int);
 private:
 	string enemy_Name;//敌人名字
 	int Hp;//血量
+	int Hp_new;
 	int Atk;//攻击力
 	int Def;//防御力
 	int Money_drop;//击败掉落金钱
 	int Exp_drop;//掉落经验
 	int Items_drop;//掉落物品
-	int id;
+	int id;//怪物编号
 };
