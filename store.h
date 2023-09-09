749e570d7215b5ed0cd0edf0009812cf95a55f15
daydreamyyrr <118284831+daydreamyyrr@users.noreply.github.com>
2023-09-09 23:15:30 +0800

    Update store.h

--- store.h
+++ store.h
@@ -2,9 +2,8 @@
 using namespace std;
 class store
 {
-public:
-	store();
+public: int get_needmoney(int);//获取需要的价钱
 protected:
-	int needmoney[19] = { 450,300,500,450,500,400,250,350,400,100,200,350,500,700,30,20,120,30,30 };
-	bool st[19] = { 0 };
+	int needmoney[22] = { 450,300,500,450,500,400,250,350,400,100,200,350,500,700,30,80,20,20,120,100,30,30 };
+	bool st[22] = { 0 };
 };
