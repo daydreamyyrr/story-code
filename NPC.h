4e5021fe462f82b2d61e65d75775bcde96610feb
daydreamyyrr <118284831+daydreamyyrr@users.noreply.github.com>
2023-09-09 23:12:41 +0800

    Update NPC.h

--- NPC.h
+++ NPC.h
@@ -1,17 +1,14 @@
-#pragma once
-#include<iostream>
-
+#include"fight.h"
 using namespace std;
 
 class Npc
 {
 public:
 	Npc() {};
-	~Npc(){};
-	void NpcChat(int position, int StoryFlag);//npc任务剧情,position为人物当前位置，flag为任务节点，在fight中
-	void StoryPs(int position, int StoryFlag);//任务提示
+	~Npc() {};
+	void NpcChat(int position,Role&);//npc任务剧情,position为人物当前位置，flag为任务节点，在fight中
+	void StoryPs(int StoryFlag);//任务提示
 protected:
 	//string name;
-	
-};
 
+};
